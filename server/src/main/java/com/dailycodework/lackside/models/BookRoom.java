b0b3e9836d0b0fb9cd7b32fc7f1ba819e63bfd9b
Alphanumeric Str <150432237+numericar@users.noreply.github.com>
2025-06-09 09:50:21 +0700

    update model

--- server/src/main/java/com/dailycodework/lackside/models/BookRoom.java
+++ server/src/main/java/com/dailycodework/lackside/models/BookRoom.java
@@ -1,10 +1,73 @@
 package com.dailycodework.lackside.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "book_rooms")
 public class BookRoom {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "check_in_date_time")
+    private LocalDateTime checkInDateTime;
+
+    @Column(name = "check_out_date_time")
+    private LocalDateTime checkOutDateTime;
+
+    @Column(name = "guest_full_name", length = 100, nullable = false)
+    private String guestFullName;
+
+    @Column(name = "guest_email", length = 100, nullable = false)
+    private String guestEmail;
+
+    @Column(name = "number_of_adults")
+    private int numberOfAdults;
+
+    @Column(name = "number_of_children")
+    private int numberOfChildren;
+    
+    @Column(name = "total_number_of_guest")
+    private int totalNumberOfGuest;
+
+    @Column(name = "booking_confirmation_code")
+    private String bookingConfirmationCode;
+
+    @JoinColumn(name = "room_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Room room;
+
+    public void calculateNumberOfGuest() {
+        this.totalNumberOfGuest = this.numberOfAdults + this.numberOfChildren;
+    }
+
+    public void setNumberOfAdults(int numberOfAdults) {
+        this.numberOfAdults = numberOfAdults;
+
+        this.calculateNumberOfGuest();
+    }
+
+    public void setNumberOfChildren(int numberOfChildren) {
+        this.numberOfChildren = numberOfChildren;
+
+        this.calculateNumberOfGuest();
+    }
 }
--- server/src/main/java/com/dailycodework/lackside/models/Room.java
+++ server/src/main/java/com/dailycodework/lackside/models/Room.java
@@ -1,12 +1,63 @@
 package com.dailycodework.lackside.models;
 
+import java.math.BigDecimal;
+import java.sql.Blob;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 @Entity
 @Table(name = "rooms")
 public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "room_type", length = 100, nullable = false)
+    private String roomType;
+
+    @Column(name = "room_price", nullable = false)
+    private BigDecimal roomPrice;
+
+    @Column(name = "is_booking", nullable = false)
+    private boolean isBooked = false;
+
+    @Lob
+    @Column(name = "photo")
+    private Blob photo;
+
+    @OneToMany(mappedBy = "room", fetch = FetchType.LAZY)
+    private List<BookRoom> bookings;
+
+    public Room() {
+        this.bookings = new ArrayList<>();
+    }
+
+    public void addBooking(BookRoom booking) {
+        if (this.bookings == null) {
+            this.bookings = new ArrayList<>();
+        } 
+
+        booking.setRoom(this);
+        this.isBooked = true;
+        String confirmationCode = UUID.randomUUID().toString();
+        booking.setBookingConfirmationCode(confirmationCode);
+        this.bookings.add(booking);
+    }
 }
